e764ae491b3a9aeab1f742e4ef986f590578ee6b
Sheng Yunzhou <shengyunzhou@gmail.com>
2022-05-31 17:03:52 +0800

    update index page with more descriptions

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,8 +2,8 @@ export default function Index() {
   return (
     <div>
       <h1 className="text-2xl font-bold">Hi, I'm Sheng Yunzhou.</h1>
-      <p className="my-4">Most people call me YZ.</p>
-      <p>I am a Software Engineer based in Singapore.</p>
+      <p className="my-4">Most people call me YZ <span className="italic">(short for Yunzhou, which is rather challenging to pronounce if you do not speak Chinese)</span>.</p>
+      <p>I am a Software Engineer based in Singapore. I enjoy writing client facing apps on web, iOS and Android. I'm also facinated by Clojure.</p>
     </div>
   );
 }
