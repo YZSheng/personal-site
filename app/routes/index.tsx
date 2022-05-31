002e57751eb25d0530052a0fdf97e61df1bd9de4
Sheng Yunzhou <shengyunzhou@gmail.com>
2022-05-31 14:10:53 +0800

    initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,11 @@
+export default function Index() {
+  return (
+    <div className="container mx-auto py-8 max-w-5xl">
+      <div className="content">
+        <h1 className="text-2xl font-bold">Hi, I'm Sheng Yunzhou.</h1>
+        <p className="my-4">Most people call me YZ.</p>
+        <p>I am a Software Engineer based in Singapore.</p>
+      </div>
+    </div>
+  );
+}
