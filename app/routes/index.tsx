69e8a560bc9306b9e94acc046db5aa84cae7ae47
Sheng Yunzhou <shengyunzhou@gmail.com>
2022-05-31 16:04:23 +0800

    update sylte with more padding

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
 export default function Index() {
   return (
-    <div className="container mx-auto py-8 max-w-5xl">
+    <div className="container mx-auto py-8 px-4 max-w-5xl">
       <div className="content">
         <h1 className="text-2xl font-bold">Hi, I'm Sheng Yunzhou.</h1>
         <p className="my-4">Most people call me YZ.</p>
