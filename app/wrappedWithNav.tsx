f994e6785a862551736f9a1c1b5adddecb4bf53a
Sheng Yunzhou <shengyunzhou@gmail.com>
2022-06-01 09:23:48 +0800

    attempt to address min-height problem on ios browser

--- app/wrappedWithNav.tsx
+++ app/wrappedWithNav.tsx
@@ -3,7 +3,7 @@ import { Link } from "@remix-run/react";
 const WrappedWithNav = ({ children }: { children: React.ReactNode }) => {
   const menuStyle = `py-3 text-sm text-gray-500 hover:text-gray-800 mr-6 inline-block`;
   return (
-    <div className="container flex min-h-screen flex-col mx-auto px-8 max-w-5xl sm:px-16">
+    <div className="container flex min-h-screen flex-col mx-auto px-8 max-w-5xl sm:px-16 ios-full-height">
       <div className="flex-grow mt-8">{children}</div>
       <div className="flex py-4 border-t border-gray-400">
         <Link to="/" className={menuStyle}>
