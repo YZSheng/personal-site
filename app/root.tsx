5ded8cc7d08340cda768dbccf620a34f59d2090c
Sheng Yunzhou <shengyunzhou@gmail.com>
2022-06-01 09:05:25 +0800

    add ga tags

--- app/root.tsx
+++ app/root.tsx
@@ -6,9 +6,12 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLocation,
 } from "@remix-run/react";
+import { useEffect } from "react";
 import styles from "./styles/app.css";
 import WrappedWithNav from "./wrappedWithNav";
+import * as gtag from "~/utils/ga.client";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -20,7 +23,17 @@ export function links() {
   return [{ rel: "stylesheet", href: styles }];
 }
 
+const gaTrackingId = "G-B9LK0EFDSB";
+
 export default function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (gaTrackingId?.length) {
+      gtag.pageview(location.pathname, gaTrackingId);
+    }
+  }, [location, gaTrackingId]);
+
   return (
     <html lang="en">
       <head>
@@ -28,6 +41,29 @@ export default function App() {
         <Links />
       </head>
       <body>
+        {process.env.NODE_ENV === "development" || !gaTrackingId ? null : (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+            />
+            <script
+              async
+              id="gtag-init"
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', '${gaTrackingId}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+              }}
+            />
+          </>
+        )}
         <WrappedWithNav>
           <Outlet />
         </WrappedWithNav>
