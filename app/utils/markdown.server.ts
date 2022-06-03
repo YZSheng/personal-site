a57e1e592a289d8032700cf0fb79c030ad964c65
Sheng Yunzhou <shengyunzhou@gmail.com>
2022-06-03 14:02:38 +0800

    update highlight.js version

--- app/utils/markdown.server.ts
+++ app/utils/markdown.server.ts
@@ -3,12 +3,9 @@ import { marked } from "marked";
 export const parseMarkdownWithHighlight = (md: string): string => {
   marked.setOptions({
     highlight: function (code, lang) {
-      const hljs = require("highlight.js");
-      if (hljs.getLanguage(lang)) {
-        return hljs.highlight(lang, code).value;
-      } else {
-        return hljs.highlightAuto(code).value;
-      }
+      const hljs = require('highlight.js');
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+      return hljs.highlight(code, { language }).value;
     },
   });
   return marked.parse(md);
