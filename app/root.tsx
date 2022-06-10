ae46be99fe08bff017406acec7da144509500f3e
Sheng Yunzhou <shengyunzhou@gmail.com>
2022-06-10 18:36:57 +0800

    Add meta description tag

--- app/root.tsx
+++ app/root.tsx
@@ -16,6 +16,7 @@ import WrappedWithNav from "./wrappedWithNav";
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Yunzhou's Personal Site",
+  description: "Welcome to Yunzhou's site",
   viewport: "width=device-width,initial-scale=1",
 });
 
